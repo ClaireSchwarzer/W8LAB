ce003bc9923864ba735870a570edceebb6cf35e2
laiqixiao <laiqixiao214@gmai.com>
2024-03-06 16:52:19 -0500

    Test pop method, passed

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -19,5 +19,12 @@ public class AppTest
         assertFalse(stack.isEmpty());//Stack is not empty after push
         assertEquals(Integer.valueOf(1), stack.get(stack.size() - 1));//top element is the one last pushed
     }
+    public void testPop() {
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(1);
+        Integer i = stack.pop();
+        assertEquals(Integer.valueOf(1), i);//Pop should return the i
+        assertTrue(stack.isEmpty());//Stack should be empty after popping the only element
+    }
 
 }
