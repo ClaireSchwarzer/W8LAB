31301ddc26a3306a16af95ca9d8e44d6e2dcd8ad
laiqixiao <laiqixiao214@gmai.com>
2024-03-06 18:17:20 -0500

    Step1 creating an instance variable of type ArrayList , initialized as "this" in the constructor of MyStack. All test passed.

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -4,15 +4,19 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class MyStack<E> extends ArrayList<E> {
+    private ArrayList<E> delegate;
+    public MyStack() {
+        this.delegate = this; // step1
 
+    }
     public void push(E e) {
-        add(e);
+        delegate.add(e);
     }
 
     public E pop() {
-        if (isEmpty()) throw new EmptyStackException();
-        E e = get(size() - 1);
-        remove(size() -1 );
+        if (delegate.isEmpty()) throw new EmptyStackException();
+        E e = delegate.get(delegate.size() - 1);
+        delegate.remove(delegate.size() -1 );
         return e;
     }
 
